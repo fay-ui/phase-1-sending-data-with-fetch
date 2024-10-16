6a5a021ec2db8333c2b54f9c3d35359fb095cef9
faith wangari njau <faithjonnes91@gmail.com>
2024-10-16 21:16:46 +0300

    add text

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+        name: name,
+        email: email
+    };
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        const responseDiv = document.getElementById('response');
+        responseDiv.innerHTML = `User ID: ${data.id}`;
+    })
+    .catch(function(error) {
+        const responseDiv = document.getElementById('response');
+        responseDiv.innerHTML = `Error: ${error.message}`;
+    });
+}
